3daf4e7f2244237bdb7414a1b7a126a010f35590
Jeferson Costa <jeferson_cta@hotmail.com>
2024-02-26 21:23:52 -0300

    Garantindo que o Tailwind está funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
+      <div className='flex min-h-screen min-w-screen bg-zinc-900 text-zinc-400 items-center justify-center'>
+        <h1>Hello Tailwind</h1>
       </div>
     </>
   )
